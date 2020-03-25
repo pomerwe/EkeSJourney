70a4e077d3bc205cd1e8cef879189651761810dc
Michael <hyuzaky@gmail.com>
2020-03-25 18:06:32 -0300

    olha o movimento

---
+++ teste 01/Assets/Scripts/Player.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float Speed;
+    public float JumpForce;
+
+    public bool isjumping;
+    public bool doublejump;
+
+    private Rigidbody2D rig;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+    void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime * Speed;
+    }
+    void Jump()
+    {
+        if(Input.GetButtonDown("Jump"))
+        {
+            if(!isjumping)
+            {
+                rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                doublejump = true;
+            }
+            else
+            {
+                if(doublejump)
+                {
+                    rig.AddForce(new Vector2(0f, JumpForce), ForceMode2D.Impulse);
+                    doublejump = false;
+                }
+
+            }
+            
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            isjumping = false;
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 8)
+        {
+            isjumping = true;
+        }
+    }
+    
+}
