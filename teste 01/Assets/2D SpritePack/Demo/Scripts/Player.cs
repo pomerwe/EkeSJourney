03df76557a5ae190c4e560f8e4707f8d8fd4fccb
Estevão <pomerwe@gmail.com>
2020-03-26 20:54:23 -0300

    Player fixes

--- teste 01/Assets/2D SpritePack/Demo/Scripts/Player.cs
+++ teste 01/Assets/2D SpritePack/Demo/Scripts/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Ninja : MonoBehaviour {
 	private Rigidbody2D myrigidbody;
 	[SerializeField]
 	private float movementspeed = 10f;
