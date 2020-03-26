7430adc84558e170f4e699f9cad011194599f362
Estevão <pomerwe@gmail.com>
2020-03-26 16:58:39 -0300

    Movement

---
+++ tentandpo um mmo rpg sandbox/Assets/CameraScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+
+    public GameObject maria;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = maria.transform.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        float desiredAngle = maria.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        var pos = maria.transform.position - (rotation * offset);
+        pos.y = pos.y + 10;
+        transform.position = pos;
+        transform.LookAt(maria.transform);
+    }
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
---
+++ tentandpo um mmo rpg sandbox/Assets/MariaScript.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MariaScript : MonoBehaviour
+{
+    public int movementSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.S))
+        {
+            var pos = transform.position;
+            pos.z += 1 * movementSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            var pos = transform.position;
+            pos.z -= 1 * movementSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            var pos = transform.position;
+            pos.x += 1 * movementSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            var pos = transform.position;
+            pos.x -= 1 * movementSpeed * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
