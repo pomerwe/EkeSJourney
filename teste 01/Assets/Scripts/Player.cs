61ac3354c5cc61b65e2d0a81ca66ceca7c915ae2
Estevão <pomerwe@gmail.com>
2020-03-26 23:09:56 -0300

    player movements

--- teste 01/Assets/Scripts/Player.cs
+++ teste 01/Assets/Scripts/Player.cs
@@ -13,13 +13,18 @@ public class Player : MonoBehaviour
     public bool isjumping;
     public bool doublejump;
 
+    [SerializeField]
+    private LayerMask groundLayer;
+
     private Rigidbody2D rig;
+    private CapsuleCollider2D capsCollider;
     private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        capsCollider = GetComponent<CapsuleCollider2D>();
         anim = GetComponent<Animator>();
     }
     // Update is called once per frame
@@ -57,13 +62,13 @@ public class Player : MonoBehaviour
     }
     void Jump()
     {
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetKeyDown(KeyCode.W))
         {
-           
             if (!isjumping)
             {
                 ResetYForce();
                 rig.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+                isjumping = true;
                 anim.SetBool("jump", true);
             }
             else
@@ -81,9 +86,47 @@ public class Player : MonoBehaviour
         }
     }
 
+
+    private bool IsGrounded()
+    {
+        float extraHeighText = 0.01f;
+        RaycastHit2D raycastHit = Physics2D.Raycast(capsCollider.bounds.center, Vector2.down, capsCollider.bounds.extents.y + extraHeighText, groundLayer);
+        Color rayColor = Color.green;
+        if (raycastHit.collider != null)
+        {
+           return true;
+        }
+        return false;
+    }
+
+    private bool IsAttachedToWall()
+    {
+        float extraHeighText = 0.01f;
+        RaycastHit2D raycastHitLeft = Physics2D.Raycast(capsCollider.bounds.center, Vector2.left, capsCollider.bounds.extents.y + extraHeighText, groundLayer);
+        RaycastHit2D raycastHitRight = Physics2D.Raycast(capsCollider.bounds.center, Vector2.right, capsCollider.bounds.extents.y + extraHeighText, groundLayer);
+        Color rayColor = Color.green;
+        if (raycastHitLeft.collider != null || raycastHitRight.collider != null)
+        {
+           return true;
+        }
+        return false;
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(IsAttachedToWall())
+        {
+            
+        }
+    }
+    void OnCollisionExit2D(Collision2D collision)
+    {
+       
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.layer == 8)
+        if (IsGrounded())
         {
             isjumping = false;
             doublejump = false;
@@ -92,14 +135,7 @@ public class Player : MonoBehaviour
             ResetYForce();
         }
     }
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.layer == 8)
-        {
-            isjumping = true;
-        }
-    }
-    
+
     void OnTriggerEnter2D(Collider2D  coll)
     {
         if (coll.gameObject.GetComponent<GenericItem>() != null)
---
+++ teste 01/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public GameObject player;
+
+
+    public float xMax;
+    public float xMin;
+    public float yMax;
+    public float yMin;
+    private float cameraSpeed = 0.15f;
+    private float urgencySpeed = 8f;
+    public bool urgencyCamera = false;
+
+    //Mathf.Clamp(player.transform.position.x,xMin,xMax)
+    public int currentY;
+    public int currentX;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Math.Abs(transform.position.y - player.transform.position.y) > 2)
+        {
+            currentY = (int)player.transform.position.y + 2;
+            if(Math.Abs(transform.position.y - player.transform.position.y) > 8)
+            {
+                urgencyCamera = true;
+            }
+            else
+            {
+                urgencyCamera = false;
+            }
+        }
+
+        if (Math.Abs(transform.position.x - player.transform.position.x) > 1)
+        {
+            currentX = (int)player.transform.position.x;
+        }
+
+        var pos = transform.position;
+
+        //if (currentX > (int)pos.x)
+        //{
+        //    pos.x += cameraSpeed;
+        //}
+        //else if (currentX < (int)pos.x)
+        //{
+        //    pos.x -= cameraSpeed;
+        //}
+
+        pos.x = player.transform.position.x + 1;
+        if (currentY > (int)pos.y)
+        {
+            pos.y += !urgencyCamera ? cameraSpeed : cameraSpeed * urgencySpeed;
+        }
+        else if (currentY < (int)pos.y)
+        {
+            pos.y -= !urgencyCamera ? cameraSpeed : cameraSpeed * urgencySpeed;
+        }
+        transform.position = pos;
+        //transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+
+
+    }
+}
